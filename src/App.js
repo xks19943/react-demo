65050d24217a4cf3e0e7cd6723d9327b7d59ab0b
mingming <xks1994@gmail.com>
2018-12-27 14:24:24 +0800

    完成启动redux插件的配置

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export default class App extends Component {
+  render(){
+    const {store,increment,decrement,incrementAsync} = this.props;
+    return(
+      <div>
+        <h1>共有${store.getState()}把机关枪</h1>
+        <button onClick={()=>store.dispatch(increment())}>加机关枪</button>
+        <button onClick={()=>store.dispatch(decrement())}>减机关枪</button>
+        <button onClick={()=>store.dispatch(incrementAsync())}>延迟上交机关枪</button>
       </div>
-    );
+    )
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDom from 'react-dom';
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {counter, increment, decrement, incrementAsync} from "./index.redux";
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 创建 Redux store 来存放应用的状态。
+// API 是 { subscribe, dispatch, getState }。
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//是否开启chrome中redux的插件
+const reduxDevTools = window.devToolsExtension ? window.devToolsExtension() : ()=>{};
+
+//compose组合多个中间件函数
+const store = createStore(counter, compose(
+    applyMiddleware(thunk),
+    reduxDevTools
+));
+
+
+function render() {
+  ReactDom.render(
+    <App
+      store={store}
+      increment={increment}
+      decrement={decrement}
+      incrementAsync={incrementAsync}/>,
+    document.getElementById('root'));
+}
+
+render();
+
+//当state变化的时候触发重新渲染的方法
+store.subscribe(()=>{
+  render();
+});
