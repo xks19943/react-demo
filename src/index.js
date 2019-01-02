491b0dbfe934aea4ba4def08bbecc01dc7804e76
mingming <xks1994@gmail.com>
2019-01-02 16:27:44 +0800

    完成tododemo

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-
 import React from 'react';
 import ReactDom from 'react-dom';
-import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import {Provider} from 'react-redux';
-import {counter} from "./index.redux";
-
-import App from './App';
-
-// 创建 Redux store 来存放应用的状态。
-// API 是 { subscribe, dispatch, getState }。
-
-//是否开启chrome中redux的插件
-const reduxDevTools = window.devToolsExtension ? window.devToolsExtension() : ()=>{};
-
-//compose组合多个中间件函数
-const store = createStore(counter, compose(
-    applyMiddleware(thunk),
-    reduxDevTools
-));
-
+import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+let store = createStore(reducers);
 
 ReactDom.render(
   <Provider store={store}>
     <App/>
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
